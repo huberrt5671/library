7ebe32ab09c8818c04ef40026e2ed46ef9a858a1
huberrt5671 <hubernotis34@gmail.com>
2025-02-11 03:14:18 +0900

    first commit

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="root-container flex min-h-screen flex-col items-center justify-center">
+        <h1 className="font-bebas-neue text-5xl font-bold text-light-100">Whoa, Slow Down There, Speedy!</h1>
+        <p className="text-center mt-3 max-w-xl text-light-400">
+            Looks like you have been a little too eager. we have put a temporary
+            pause on your excitment. Chill for a bit and try again shorthly.
+        </p>
+    </main>
+  )
+}
+
+export default page
